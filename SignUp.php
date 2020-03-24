7d55015093fa5cc816ed206947e1fbd4ccd36a60
KarthikUKumar <55427841+KarthikUKumar@users.noreply.github.com>
2020-03-24 21:09:26 +0530

    Update SignUp.php

--- SignUp.php
+++ SignUp.php
@@ -1,28 +1,31 @@
 <?php
-$d=pg_connect("host=ec2-23-22-156-110.compute-1.amazonaws.com port=5432 dbname=dc71h5v4qsc5iq user=dmnsyiybmedxbz password=943ba26baf8eb1c6c0898f6e8771e492807a6ed312e5351c7c8d54806ac000c0 sslmode=require") or die ("ERROR connecting to Cloud database!");
-$gst=pg_escape_string($_POST["gstin"]);
-$company=pg_escape_string($_POST["company"]);
-$email=pg_escape_string($_POST["uemail"]);
-$address=pg_escape_string($_POST["address"].$_POST["address2"]);
-$State=pg_escape_string($_POST["State"]);
-$Country=pg_escape_string($_POST["Country"]);
-$phone=pg_escape_string($_POST["Phone"]);
-$pw=pg_escape_string($_POST["pwd1"]);
-if(isset($_POST["UEmail"]) && isset($_POST["Company"]) && !empty($_POST["pwd1"]))
+try
 {
-	
-    if(pg_query($d,"Insert into User_Detail values(default,'$gst','$email',sha1('$pw'),'$address','$State','$Country','$phone')"))
-    {
-    	print("Created Successfully.");
+	$pdo=new PDO("pgsql:host=ec2-23-22-156-110.compute-1.amazonaws.com;port=5432;dbname=dc71h5v4qsc5iq","dmnsyiybmedxbz","943ba26baf8eb1c6c0898f6e8771e492807a6ed312e5351c7c8d54806ac000c0");
+	$gst=$_POST["gstin"];
+    $company=$_POST["company"];
+    $email=$_POST["uemail"];
+    $address=$_POST["address"].$_POST["Address2"];
+    $State=$_POST["State"];
+    $Country=$_POST["Country"];
+    $phone=$_POST["Phone"];
+    $pw="'".$_POST["pwd1"]."'";
+    $sql="Insert into User_Detail (gstin_no,uemail,password,company,uaddress,ustate,ucountry,phone_no) values (?,?,?,?,?,?,?,?)";
+    $stmt=$pdo->prepare($sql);
+    $stmt->execute([$gst,$email,sha1($pw),$company,$address,$State,$Country,$phone]);
+    if($stmt->errorCode()==0){
+    	echo "Inserted Successfully";
         //header('location:LoginPageI.php');
-        //exit; 
+        //exit;  
     }
-    else
-    {
-    	print("error");
-        //header('location:SignUpI.php');
-        //exit;    
-    }    
-    pg_close($d);
+    else{
+    	echo $stmt->errorCode()."Sorry! there was an internal error";
+    	//header('location:SignUpI.php');
+        //exit;
+    }  
+}
+catch(PDOException $e){
+	echo $e->getMessage();
+	$pdo=null;
 }
 ?>
