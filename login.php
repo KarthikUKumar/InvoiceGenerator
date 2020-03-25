87de888e3d9700e924118c9b27487364054c948b
KarthikUKumar <55427841+KarthikUKumar@users.noreply.github.com>
2020-03-25 17:48:41 +0530

    Update login.php

--- login.php
+++ login.php
@@ -1,37 +1,41 @@
 <?php
 session_start();
-$d=pg_connect("host=ec2-23-22-156-110.compute-1.amazonaws.com port=5432 dbname=dc71h5v4qsc5iq user=dmnsyiybmedxbz password=943ba26baf8eb1c6c0898f6e8771e492807a6ed312e5351c7c8d54806ac000c0 sslmode=require") or die("Error connecting to Cloud");
-$e=pg_escape_string($_POST["email"]);
-$p=pg_escape_string($_POST["password"]);
-if(isset($_POST["email"]) && isset($_POST["password"]))
+try
 {
-    $pasw=pg_query($d,"select Password,UserId,UEmail from User_Details where UEmail='$e'");
-    if(pg_num_rows($pasw)<0)
-    {
-    	echo "<h1>Invalid Email-Id</h1>";
+	$pdo=new PDO("pgsql:host=ec2-23-22-156-110.compute-1.amazonaws.com;port=5432;dbname=dc71h5v4qsc5iq","dmnsyiybmedxbz","943ba26baf8eb1c6c0898f6e8771e492807a6ed312e5351c7c8d54806ac000c0");
+	$em=$_POST["email"];
+    $pas=$_POST["password"];
+    $sql="select Password,UserId,UEmail from User_Detail where UEmail=? Limit 1";
+    $stmt=$pdo->prepare($sql);
+    $stmt->execute([$em]);
+    if($stmt->rowCount()!=0){
+    	$res=$stmt->fetch();
+    	if($res['password']==md5($pas))
+    	{
+    		echo "Successfully logged in.";
+    		$_SESSION["UserId"]=$res['userid'];
+    		//header('location:LoginPageI.php');
+            //exit;uncomment these Faraz
+    	}
+    	else{
+    		session_unset();
+    		session_destroy();
+    		echo "Incorrect password!";
+    		//header('location:LoginPageI.php');
+            //exit;uncomment these Faraz
+    	}
     }
-    else
-    {
-    	$psw=pg_fetch_array($pasw,0,PGSQL_NUM);
-	    if(sha1($p)==$psw[3])
-	    {
-	    	$_SESSION["UserId"]=$psw[0];
-	    	printf("Login Successful.and UserId=%d",$_SESSION['UserId']);
-            //header("Location:WebHome.php");
-            //exit;
-        }
-        else
-        {
-        	session_unset();
-        	session_destroy();
-	        //header("Location:LoginPageI1.php");
-            //exit;
-        }
+    else{
+    	session_unset();
+    	session_destroy();
+    	echo "Please create an account ";//add a link here Faraz.
     }
+
 }
-else
-{
-	echo "";
-}
-pg_close($d);
+catch(PDOException $e){
+	session_unset();
+    session_destroy();
+	echo "Connection Failed\n".$e->getMessage();
+	$pdo=null;
+} 
 ?>
