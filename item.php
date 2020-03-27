2529800b802b40e500337432a4c552a1bb38a700
KarthikUKumar <55427841+KarthikUKumar@users.noreply.github.com>
2020-03-27 16:16:15 +0530

    Update item.php

--- item.php
+++ item.php
@@ -10,7 +10,7 @@ session_start();
     <SCRIPT language="javascript" src="rowadd.js"></SCRIPT>
 </HEAD>
 <BODY><center>
-   <h1>Welcome <? echo $_SESSION["Company"]; ?></h1>
+   <h1>Welcome <? echo '$_SESSION["Company"]'; ?></h1>
    <h4>Add Items</h4></center>
     <div class="container">
     <form id="form1" action="itemadd.php" method="post">
@@ -25,8 +25,8 @@ session_start();
         <TR>
             <TD class="col-sm-1"><INPUT type="checkbox" name="chk[]"></TD>
             <TD class="col-sm-3"><INPUT type="text" name="nam[]" class="form-control"></TD>
-            <td class="col-sm-4"><textarea row="5" cols="50" form="form1" name="desc[]" class="form-control"></textarea></td>
-            <td class="col-sm-2"><input name="price[]" pattern="^\d*(\.\d{0,2})?$" form="form1" class="form-control">
+            <td class="col-sm-4"><textarea row="0" cols="50" form="form1" name="desc[]" class="form-control"></textarea></td>
+            <td class="col-sm-2"><input name="price[]" pattern="^\d*(\.\d{0,2})?$" form="form1" class="form-control"></td>
             <TD class="col-sm-2">
                 <SELECT name="gst[]" form="form1" class="form-control">
                     <OPTION value="0" class="form-control">Nil</OPTION>
@@ -38,9 +38,12 @@ session_start();
             </TD>
         </TR>
     </TABLE>
-    <INPUT type="button" value="Add Row +" onclick="addRow('dataTable')" class="btn btn-md btn btn-info col-sm-4">
-    <INPUT type="button" value="Delete Row -" onclick="deleteRow('dataTable')" class="btn btn-md btn btn-danger col-sm-4">
-    <input type="submit" value="Submit" class="btn btn-md btn btn-success col-sm-4">
-</td>
+    <div class="col-sm-1"></div>
+    <INPUT type="button" value="Add Row +" onclick="addRow('dataTable')" class="btn btn-md btn btn-info col-sm-3">
+    <div class="col-sm-1"></div>
+    <INPUT type="button" value="Delete Row -" onclick="deleteRow('dataTable')" class="btn btn-md btn btn-danger col-sm-3">
+    <div class="col-sm-1"></div>
+    <input type="submit" value="Submit" class="btn btn-md btn btn-success col-sm-3">
+</div>
 </BODY>
 </HTML>
