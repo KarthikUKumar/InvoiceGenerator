f534fcfbc2ab191167c63100f124183684c62e28
KarthikUKumar <55427841+KarthikUKumar@users.noreply.github.com>
2020-04-08 21:00:50 +0530

    Create rowadd.js

---
+++ js/rowadd.js
@@ -0,0 +1,70 @@
+function addRow(tableID) {
+
+            var table = document.getElementById(tableID);
+
+            var rowCount = table.rows.length;
+            var row = table.insertRow(rowCount);
+
+            var colCount = table.rows[1].cells.length;
+
+            for(var i=0; i<colCount; i++) {
+
+                var newcell = row.insertCell(i);
+
+                newcell.innerHTML = table.rows[1].cells[i].innerHTML;
+                switch(newcell.childNodes[0].name) {
+                    case "nam":
+                            newcell.childNodes[0].value = "";
+                            break;
+                    case "gst":
+                            newcell.childNodes[0].selectedIndex = 0;
+                            break;
+                    case "desc":
+                            newcell.childNodes[0].value = "";
+                            break;
+                    case "chk":
+                            newcell.childNodes[0].checked = false;
+                            break;
+                    case "price":
+                            newcell.childNodes[0].value = 0.00;
+                            break;
+                }
+            }
+        }
+
+        function deleteRow(tableID) {
+            try {
+            var table = document.getElementById(tableID);
+            var rowCount = table.rows.length;
+
+            for(var i=0; i<rowCount; i++) {
+                var row = table.rows[i];
+                var chkbox = row.cells[0].childNodes[0];
+                if(null != chkbox && true == chkbox.checked) {
+                    if(rowCount <= 2) {
+                        alert("Cannot delete all the rows.");
+                        break;
+                    }
+                    table.deleteRow(i);
+                    rowCount--;
+                    i--;
+                }
+
+
+            }
+            }catch(e) {
+                alert(e);
+            }
+        }
+$(document).on('keydown', 'input[pattern]', function(e){
+  var input = $(this);
+  var oldVal = input.val();
+  var regex = new RegExp(input.attr('pattern'), 'g');
+
+  setTimeout(function(){
+    var newVal = input.val();
+    if(!regex.test(newVal)){
+      input.val(oldVal); 
+    }
+  }, 0);
+});
