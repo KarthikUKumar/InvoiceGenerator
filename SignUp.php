88d4d3a67e44fa998b8fae7f5804090e62a732ba
KarthikUKumar <55427841+KarthikUKumar@users.noreply.github.com>
2020-04-25 11:20:03 +0530

    Add files via upload

--- SignUp.php
+++ SignUp.php
@@ -8,7 +8,7 @@ try
 {
     $pdo=new PDO("pgsql:host=ec2-23-22-156-110.compute-1.amazonaws.com;port=5432;dbname=dc71h5v4qsc5iq","dmnsyiybmedxbz","943ba26baf8eb1c6c0898f6e8771e492807a6ed312e5351c7c8d54806ac000c0");
     $gst=$_POST["gstin"];
-    $pin=$_POST["PinCode"];
+    $pin=$_POST["pincode"];
     $company=$_POST["company"];
     $email=$_POST["uemail"];
     $address=$_POST["address"].", ".$_POST["Address2"];
@@ -16,16 +16,18 @@ try
     $Country=$_POST["Country"];
     $phone=$_POST["Phone"];
     $pw=$_POST["pwd1"];
-    $sql="Insert into User_Detail (gstin_no,uemail,password,company,uaddress,ustate,ucountry,phone_no,PinCode) values (?,?,?,?,?,?,?,?,?)";
-    $stmt=$pdo->prepare($sql);
-    $stmt->execute([$gst,$email,md5($pw),$company,$address,$State,$Country,$phone,$pin]);
-    if($stmt->errorCode()==0){
+    $sql="Insert into User_Detail (gstin_no,uemail,password,company,uaddress,ustate,ucountry,phone_no,pincode) values (?,?,?,?,?,?,?,?,?)";
+    try{
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $pdo->beginTransaction();
+        $stmt=$pdo->prepare($sql);
+        $stmt->execute([$gst,$email,md5($pw),$company,$address,$State,$Country,$phone,$pin]);
+        $pdo->commit();
         echo "<script> alert(\"Account Created Successfully\");
-        window.location='home.html'; </script>"; 
+                    window.location='home.html'; </script>"; 
     }
-    else{
-        $error=$stmt->errorInfo();
-        echo "<script> alert(\"Sorry! there is an error - $error \");
+    catch(PDOException $e){
+        echo "<script> alert(\"Sorry! there is an error - ".$e->getMessage()." \");
         window.location='home.html'; <script>";
     }       
 }
