2ff035d6cb2f5f3754c0c6434585fbf0f1643165
KarthikUKumar <55427841+KarthikUKumar@users.noreply.github.com>
2020-04-27 19:45:08 +0530

    Update itemview.php

--- itemview.php
+++ itemview.php
@@ -134,7 +134,7 @@ else{
   session_unset();
   session_destroy();
   echo "<script> alert(\"There was some internal server error.Please Login\");
-            window.location='index.html'; </script>";
+            window.location='home.html'; </script>";
 }
 ?>
   </body>
